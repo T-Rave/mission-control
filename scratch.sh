04afab96f3e79d1add764f358b524c55d3b54921
T-Rave <trave@MacBook-Pro.local>
2018-05-02 08:43:07 -0500

    test.csv updated example | scratch for csv columns to variables

--- scratch.sh
+++ scratch.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
-read -p "Enter a number: " NUMBER
-while [ "$NUMBER" -le 10 ];
-do
-  echo "$NUMBER"
-  ((NUMBER++))
+index=0
+
+read -e -p "Enter the path to the testing CSV: " FILEPATH
+FILEPATH=$( echo $FILEPATH | sed 's/ /\\ /' )
+eval FILEPATH=$FILEPATH
+COLUMNS=$( head -1 $FILEPATH | sed 's/[^,]//g' | wc -c | xargs)
+
+head -1 $FILEPATH | while IFS=, read -r -a arr; do 
+  while [[ "$INDEX" -le "$COLUMNS" ]]
+    do
+      echo "${arr[$index]}"
+      col="${arr[$index]}"
+      echo " col - $col"
+      ((index++))
+  done
 done
+
+
+# read -p "Enter a number: " NUMBER
+# while [ "$NUMBER" -le 10 ];
+# do
+#   echo "$NUMBER"
+#   ((NUMBER++))
+# done
+
+# COL=$( head -1 $FILEPATH)
+# echo "$COL"
